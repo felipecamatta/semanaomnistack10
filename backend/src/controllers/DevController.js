fae1bd1708d26a881db0235a327457826a68f800
Camatta <felipecamattabergi@gmail.com>
2020-01-16 16:28:42 -0300

    Desenvolvida a interface web em React

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -15,6 +15,7 @@ module.exports = {
         let dev = await Dev.findOne({ github_username });
 
         if (!dev) {
+            
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
         
             const { name = login, avatar_url, bio } = apiResponse.data;
